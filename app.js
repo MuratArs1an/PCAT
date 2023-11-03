e230d2fd9798b4ab9097309c0963bcf9062615e7
MuratArs1an <kilicarslan87@gmail.com>
2023-11-03 23:57:25 +0300

    static files upladed

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const fn = function () {
-    console.log('Function wroking');
-};
+const express = require("express");
+const path=require('path')
+const app = express();
+const port = 3000;
 
-fn()
+/* const myLogger = (req,res,next) => {
+  console.log("logged");
+  next(); //next methodunu çağırmazsak bir sonraki middleware çalışmaz
+} */
+
+//MIDDLEWARES
+app.use(express.static('public'));
+/* app.use(myLogger); */
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname,'temp/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`sunucu ${port} portunda baslatıldı`);
+});
