e87b7dd37e31aeb24dcbd7deb977b12874b9fd7b
MuratArs1an <kilicarslan87@gmail.com>
2023-11-05 22:19:08 +0300

    templated enjine added

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const path=require('path')
+const ejs=require('ejs')
 const app = express();
 const port = 3000;
+
+
+//TEMPLATE ENGİNE
+app.set('view engine', 'ejs');
 
 /* const myLogger = (req,res,next) => {
   console.log("logged");
@@ -13,7 +18,16 @@ app.use(express.static('public'));
 /* app.use(myLogger); */
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname,'temp/index.html'));
+  //res.sendFile(path.resolve(__dirname,'temp/index.html'));
+  res.render('index')
+});
+
+app.get("/about",(reg,res)=>{
+  res.render('about')
+});
+
+app.get("/add",(reg,res)=>{
+  res.render('add')
 });
 
 app.listen(port, () => {
