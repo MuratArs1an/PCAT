4a2c743d48f95768e0b34b225627678ec0447845
MuratArs1an <kilicarslan87@gmail.com>
2023-11-09 01:25:20 +0300

    photos can be added

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const path=require('path')
 const ejs=require('ejs')
 const app = express();
-const port = 3000;
 const Photo=require('./models/photo');
 const mongoose=require('mongoose')
+const fileUpload=require('express-fileupload');
+const fs=require('fs')
+
+const port = 3000;
 
 //mongoDb connect
 mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db');
@@ -22,12 +25,13 @@ app.set('view engine', 'ejs');
 app.use(express.static('public')); //static dosyalarımız tanımladık
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(fileUpload());
 /* app.use(myLogger); */
 
 //ROUTES
 app.get("/", async (req, res) => {
   //res.sendFile(path.resolve(__dirname,'temp/index.html'));
-  const photos=await Photo.find({}) //veri tabanındaki verileri aldık
+  const photos=await Photo.find({}).sort('-dateCreated') //veri tabanındaki verileri aldık
   res.render('index',{
     photos
   })
@@ -49,10 +53,22 @@ app.get("/add",(req,res)=>{
   res.render('add')
 });
 
-app.post("/photos",async (req,res)=>{ //add.ejs formunda post methoduna action olarak yazdıgımız /photos
-                                //yonlendırmesını burada yakalıyoruz
-  await Photo.create(req.body); //post metodu ile gonderilen verileri yakalıp mongoya ekledik
-  res.redirect('/')
+app.post("/photos",async (req,res)=>{ //add.ejs formunda post methoduna action olarak yazdıgımız photos yonlendırmesını burada yakalıyoruz
+
+  const uploadDir='public/uploads';
+  if(!fs.existsSync(uploadDir)){
+    fs.mkdirSync(uploadDir)
+  }
+  let uploadedImage=req.files.image
+  let uploadPath=__dirname + '/public/uploads/' + uploadedImage.name //upload edilen fotoların yuklenecıgı yol
+  uploadedImage.mv(uploadPath, async ()=>{ //mv yani move ile upload olan imageleri gonderiyoruz
+      await Photo.create({
+        ...req.body,
+        image:'/uploads/'+uploadedImage.name
+      }); //post metodu ile gonderilen verileri yakalıp mongoya ekledik
+      res.redirect('/')
+    }
+  )
 });
 
 app.listen(port, () => {
